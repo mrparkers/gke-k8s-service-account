60d93142bc90fdbb325f477c6981a0168286dd91
Michael Parker <michael@parker.gg>
2018-03-06 17:44:34 -0600

    Create kube-service-account script

---
+++ kube-service-account.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+function kubectlServiceAccount() {
+    serviceAccounts=$(gcloud auth list --format json | jq -r '. | map(.account)')
+    numberOfServiceAccounts=$(echo ${serviceAccounts} | jq '. | length')
+
+    printf "\nSelect service account (run \`gcloud auth activate-service-account\` to add to this list)\n"
+
+    for (( i = 0; i < $numberOfServiceAccounts; i++ )); do
+        printf "${i})\t%s\n" "$(echo ${serviceAccounts} | jq -r --arg i "${i}" '.[$i | tonumber]')"
+    done
+
+    echo -n "Number: "
+    read index
+
+    serviceAccount=$(echo ${serviceAccounts} | jq -r --arg i "${index}" '.[$i | tonumber]')
+
+    token=$(gcloud auth print-access-token --account=${serviceAccount})
+
+    alias kubectlt='kubectl --token=${token}'
+
+    echo "Alias \`kubectlt\` has been configured to use service account ${serviceAccount}"
+}
+
+alias kube-service-account=kubectlServiceAccount
